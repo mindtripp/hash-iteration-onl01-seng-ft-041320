0a88feecee52374f26a309d89857b2a3b90a77f5
decent-scraper-1450 <jaimealba1574@gmail.com>
2020-04-16 20:04:57 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -1,5 +1,12 @@
+# passengers = {
+# suite_a: "Amanda Presley", 
+# suite_b: "Seymour Hoffman", 
+# suite_c: "Alfred Tennyson", 
+# suite_d: "Charlie Chaplin", 
+# suite_e: "Crumpet the Elf"
+# }
+
 def select_winner(passengers)
-  # add the code snippet here!
   winner = ""
   passengers.each do |suite, name|
     if suite == :suite_a && name.start_with?("A")
@@ -8,4 +15,4 @@ def select_winner(passengers)
   end
 
   winner 
-end 
+end
